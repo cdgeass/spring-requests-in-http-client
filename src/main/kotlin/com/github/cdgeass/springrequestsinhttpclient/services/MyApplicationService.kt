1a483c0782ef5b6693989f12b91a76d737920d69
GitHub Action <action@github.com>
2022-07-26 03:13:07 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/cdgeass/springrequestsinhttpclient/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.cdgeass.springrequestsinhttpclient.services
+
+import com.github.cdgeass.springrequestsinhttpclient.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/cdgeass/springrequestsinhttpclient/services/MyProjectService.kt
+++ src/main/kotlin/com/github/cdgeass/springrequestsinhttpclient/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.cdgeass.springrequestsinhttpclient.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cdgeass.springrequestsinhttpclient.MyBundle
 
 class MyProjectService(project: Project) {
 
